ad8c171c76c8fc945bd6f8ffa6f9c874a3e1ad7f
Bruni <bruno.a.dinis96@gmail.com>
2025-02-16 13:24:03 -0300

    chore: configurações iniciais do projeto utilizando: - Gulp, gulp-imagemin, gulp-sass, gulp-uglify, gulp-watche sass.
    Adicionado o index.html ao projeto.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function styles() {
+  console.log("Compilando o sass...");
+
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/src/styles"));
+}
+
+exports.default = gulp.parallel(styles);
+exports.watch = () => {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+};
