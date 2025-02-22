5b4eff18b89cd81ccb96b1401a606f1476c166fa
Bruni <bruno.a.dinis96@gmail.com>
2025-02-21 23:51:35 -0300

    feat: add nav menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 // biome-ignore lint/complexity/useArrowFunction: <explanation>
 $(document).ready(function () {
+
+  $('.hero__container img').fadeIn(2000)
+
   $("#carousel").slick({
     autoplay: true,
     infinite: true,
@@ -7,4 +10,17 @@ $(document).ready(function () {
     fade: true,
     dots: true,
   });
+  
+// biome-ignore lint/complexity/useArrowFunction: <explanation>
+  $(window).on("scroll", function() {
+    const alturaTrailer = $('.trailer').height()
+    const posicaoAtual = $(window).scrollTop()
+
+    if (posicaoAtual > alturaTrailer) {
+      $('.header').fadeIn()
+    } else{
+      $('.header').fadeOut()
+
+    }
+  })
 });
